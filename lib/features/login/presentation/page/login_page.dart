552e4756690b51d8543fef2d5848d989f05dc5da
mfarel <mfarel@openpass.com.ar>
2024-11-09 18:22:29 -0300

    Login page

--- lib/features/login/presentation/page/login_page.dart
+++ lib/features/login/presentation/page/login_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tots_material/main.dart';
+import 'package:tots_material/utils/logos/tots_logos.dart';
+import 'package:tots_material/widgets/inputs/input_text_principal.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,8 +13,55 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Bienvenido al login'),
+    TextEditingController email = TextEditingController();
+    return Scaffold(
+      body: Stack(
+        children: [
+          const BackgroundForms(),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  TOTSLogo.logoMinimal,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'LOG IN',
+                  style: TotsTextStyles.loginTItle,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                InputTextPrincipal(
+                  labelText: 'Mail',
+                  controller: email,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                InputTextPrincipal(
+                  labelText: 'Password',
+                  controller: email,
+                  isPassword: true,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                ButtonPrincipal(
+                  load: false,
+                  title: 'LOG IN',
+                  onPressed: () {},
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
